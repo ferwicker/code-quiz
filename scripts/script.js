7f95872675eeae1e7a9e93ea9bea0714194a1603
Fer Wicker <fer.wicker@gmail.com>
2020-12-10 07:22:38 +1100

    Cleaning up the script.

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,21 @@
 // main screen elements
-var screen1 = document.getElementById('start-screen');
-var screen2 = document.getElementById('questions-screen');
-var screen3 = document.getElementById('game-over-screen')
-var screen4 = document.getElementById('score-screen');
+var screen1 = document.querySelector('#start-screen');
+var screen2 = document.querySelector('#questions-screen');
+var screen3 = document.querySelector('#game-over-screen')
+var screen4 = document.querySelector('#score-screen');
 
 //screen 1
-var startButton = document.getElementById('start-button');
+var startButton = document.querySelector('#start-button');
 
 //screen 2
-var scoreEl = document.getElementById('score-counter');
-var timeEl = document.getElementById('time-left');
+var scoreEl = document.querySelector('#score-counter');
+var timeEl = document.querySelector('#time-left');
 
-var quitButton = document.getElementById('quit-button');
+var quitButton = document.querySelector('#quit-button');
 
 //screen 3
-var scoreTotalEl = document.getElementById('score-total');
-var userName = document.getElementById('name');
+var scoreTotalEl = document.querySelector('#score-total');
+var userName = document.querySelector('#name');
 
 
 
@@ -81,8 +81,8 @@ function startGame(){
 
 //question elements
 
-var questionNumEl = document.getElementById('question-number');
-var questionTextEl = document.getElementById('question-text');
+var questionNumEl = document.querySelector('#question-number');
+var questionTextEl = document.querySelector('#question-text');
 var answersCont = document.querySelector('#answers-container');
 
 let q = 0;
@@ -98,7 +98,7 @@ function scoreIncrease(){
 //display questions from object array // create and append elements for questions, do a loop inside?
 
 function displayQuestion(){
-    answersCont.textContent = '';
+    answersCont.textContent = ''; //clearing previous answers
     if (q < allQuestions.length) {
         questionNumEl.textContent = allQuestions[q]['number'] + '. ';
         questionTextEl.textContent = allQuestions[q]['question'];
@@ -111,12 +111,11 @@ function displayQuestion(){
         answersCont.appendChild(answerOp);
         };
 
-
     } else {
         clearInterval(timeInterval);
         goToScreen3();
         scoreTotalEl.textContent = scoreCounter;
-    }    
+    };   
 };
 
 
@@ -141,11 +140,8 @@ function displayMessage(message) {
 
 answersCont.addEventListener('click', function(event){
 
-    // get value of selected 'answer' 
-    var selected = event.target;
-    var val = parseInt(selected.getAttribute('data-value'));
         //check answer is correct
-        if(val === allQuestions[q]['correctAnswer']){
+        if (parseInt(event.target.getAttribute('data-value')) === allQuestions[q]['correctAnswer']) {
             scoreIncrease();
             displayMessage(correctMessage);
         } else {
@@ -160,10 +156,10 @@ answersCont.addEventListener('click', function(event){
 );
 
 // Screen 3
-let highScores = JSON.parse(localStorage.getItem("high scores") || "[]"); //are the brackets needed?
+let highScores = JSON.parse(localStorage.getItem("high scores") || '[]'); 
 
-var scoreForm = document.getElementById('score-form');
-var nameInput = document.getElementById('name');
+var scoreForm = document.querySelector('#score-form');
+var nameInput = document.querySelector('#name');
 
 
 scoreForm.addEventListener("submit", function(event) {
@@ -185,7 +181,6 @@ scoreForm.addEventListener("submit", function(event) {
     //save again to local storage
     localStorage.setItem("high scores", JSON.stringify(highScores));
 
-    nameInput.value = ""; // this is not needed if page is refreshing
     goToScreen4();
     renderHighScores();
   });
@@ -195,19 +190,19 @@ scoreForm.addEventListener("submit", function(event) {
   //render high scores --how to make this into a loop instead?
 
     var scoreBoardName = [
-        firstPlaceNameEl = document.getElementById('leaderboard-name1'), 
-        secondPlaceNameEl = document.getElementById('leaderboard-name2'),
-        thirdPlaceNameEl = document.getElementById('leaderboard-name3'), 
-        fourthPlaceNameEl = document.getElementById('leaderboard-name4'),
-        fifthPlaceNameEl = document.getElementById('leaderboard-name5'),
+        firstPlaceNameEl = document.querySelector('#leaderboard-name1'), 
+        secondPlaceNameEl = document.querySelector('#leaderboard-name2'),
+        thirdPlaceNameEl = document.querySelector('#leaderboard-name3'), 
+        fourthPlaceNameEl = document.querySelector('#leaderboard-name4'),
+        fifthPlaceNameEl = document.querySelector('#leaderboard-name5'),
     ];
 
     var scoreBoardScore = [
-        firstPlaceScoreEl = document.getElementById('leaderboard-score1'), 
-        secondPlaceScoreEl = document.getElementById('leaderboard-score2'),
-        thirdPlaceScoreEl = document.getElementById('leaderboard-score3'), 
-        fourthPlaceScoreEl = document.getElementById('leaderboard-score4'),
-        fifthPlaceScoreEl = document.getElementById('leaderboard-score5'),
+        firstPlaceScoreEl = document.querySelector('#leaderboard-score1'), 
+        secondPlaceScoreEl = document.querySelector('#leaderboard-score2'),
+        thirdPlaceScoreEl = document.querySelector('#leaderboard-score3'), 
+        fourthPlaceScoreEl = document.querySelector('#leaderboard-score4'),
+        fifthPlaceScoreEl = document.querySelector('#leaderboard-score5'),
     ];
 
 
@@ -227,18 +222,17 @@ scoreForm.addEventListener("submit", function(event) {
         }
     }
 
-    function clearScoreScreen(){ //why doesnt this work as a loop?
-            scoreBoardName[0].textContent = ''; scoreBoardScore[0].textContent = '';
-            scoreBoardName[1].textContent = ''; scoreBoardScore[1].textContent = '';
-            scoreBoardName[2].textContent = ''; scoreBoardScore[2].textContent = '';
-            scoreBoardName[3].textContent = ''; scoreBoardScore[3].textContent = '';
-            scoreBoardName[4].textContent = ''; scoreBoardScore[4].textContent = '';
+    function clearScoreScreen(){ 
+
+        for(i=0; i<5; i++){
+            scoreBoardName[i].textContent = ''; scoreBoardScore[i].textContent = '';
+        }
     }
 
   //screen 4 buttons
 
-  var playAgainButton = document.getElementById('play-again');
-  var clearScoresButton = document.getElementById('clear-scores');
+  var playAgainButton = document.querySelector('#play-again');
+  var clearScoresButton = document.querySelector('#clear-scores');
 
   function clearScores(){
     highScores = [];
